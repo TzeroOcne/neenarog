1a08c67569ba8967a5f4fab706ecca6a07c112ba
Tzero Ocne <tzeroocne.interface@gmail.com>
2026-01-10 21:04:33 +0700

    feat(theme): setup dasiyui with catpuccin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
+import db from '@astrojs/db';
+import markdoc from '@astrojs/markdoc';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  integrations: [
+    svelte(),
+    db(),
+    markdoc(),
+    mdx(),
+    partytown(),
+    sitemap()
+  ],
+  adapter: cloudflare()
+});
